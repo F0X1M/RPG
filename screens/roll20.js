816b105f697b100c9fe5b0f3ce018988a20ccc96
Wiktoria <92274494+Wiktoria345000@users.noreply.github.com>
2022-01-15 16:43:06 +0100

    [wiktoria]Final update of Dice Rolling
    Final update of Dice Rolling

--- screens/roll20.js
+++ screens/roll20.js
@@ -1,69 +1,94 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Button, Text } from 'react-native';
- 
-export default class roll20 extends Component {
- 
-  constructor(){
- 
-    super();
- 
-    this.state={
+import { useNavigation } from "@react-navigation/core";
+import { useState, useEffect } from 'react';
+import { Accelerometer } from 'expo-sensors';
 
-      NumberHolder : 1
- 
-    }
-  }
- 
-GenerateRandomNumber=()=>
-{
- 
-var RandomNumber = Math.floor(Math.random() * 20) + 1;
- 
-this.setState({
- 
-  NumberHolder : RandomNumber
- 
-})
-}
- 
-  render() {
-    return (
-   
-      <View style={styles.container} >
- 
-      <Text style={styles.text}>{this.state.NumberHolder}</Text>
- 
-      <View style={[{ width: "60%", margin: 10, backgroundColor: "red" }]}>
-        <Button 
-        onPress={this.GenerateRandomNumber}
-        title='Losuj kością'
-        color="#BBBBBB"
-        />
-      </View>
-        
-      </View>
- 
+const roll20 = () => {
+  const navigation = useNavigation();
+  const sensitivity = 3;
+  const [RandomNumber, setRandomNumber] = useState(0);
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
+
+
+  const _slow = () => {
+    Accelerometer.setUpdateInterval(1000);
+  };
+
+  const _fast = () => {
+    Accelerometer.setUpdateInterval(16);
+  };
+
+  const _subscribe = () => {
+    setSubscription(
+      Accelerometer.addListener(accelerometerData => {
+        setData(accelerometerData);
+      })
     );
-  }
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+  }, []);
+
+  var { x, y, z } = data;
+
+
+  const GenerateRandomNumber = () => {
+    if ((x > sensitivity || x < -sensitivity) &&
+      (y > sensitivity || y < -sensitivity) || (x > sensitivity || x < -sensitivity) &&
+      (z > sensitivity || z < -sensitivity) || (y > sensitivity || y < -sensitivity) &&
+      (z > sensitivity || z < -sensitivity)) {
+      setRandomNumber(Math.floor(Math.random() * 20) + 1);
+      _unsubscribe();
+      x = 0;
+      y = 0;
+      z = 0;
+      data.x = 0;
+      data.y = 0;
+      data.z = 0;
+    }
+  };
+  console.log(x, y, z);
+  return (
+    <View style={styles.container} >
+      {GenerateRandomNumber()}
+      <Text style={styles.text}>{RandomNumber}</Text>
+      <View style={[{ width: "60%", margin: 10, backgroundColor: "red" }]}>
+      </View>
+    </View>
+  );
 }
- 
+
 const styles = StyleSheet.create(
-{
-  Button:{
-    fontSize:20,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#cccccc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-        fontSize:300,
-        color:'#FFFFFF',
-        marginTop: 30,
-        fontStyle: 'italic',
-        paddingHorizontal: 10,
-        fontWeight: 'bold'
-      }
-});
+  {
+    Button: {
+      fontSize: 20,
+    },
+    container: {
+      flex: 1,
+      backgroundColor: '#cccccc',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+      fontSize: 300,
+      color: '#FFFFFF',
+      marginTop: 30,
+      fontStyle: 'italic',
+      paddingHorizontal: 10,
+      fontWeight: 'bold'
+    }
+  });
+
+export default roll20;
